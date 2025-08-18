9e95d19cd11c266c3827f9827568d4df6989fbb9
Luis Fábregas-Ibáñez <luisfabib@gmail.com>
2025-08-18 17:05:52 +0200

    condense navigation items into one menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,60 +1,87 @@
 import { Component } from '@angular/core';
-import { MenubarModule } from 'primeng/menubar';
+import { MegaMenuModule } from 'primeng/megamenu';
 import { ButtonModule } from 'primeng/button';
-import { MenuItem } from 'primeng/api';
+import { MegaMenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
-  imports: [MenubarModule, ButtonModule],
+  imports: [MegaMenuModule, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  items: MenuItem[] = [
+  items: MegaMenuItem[] = [
     {
-      label: 'Home',
+      label: 'Portfolio',
       icon: 'pi pi-home',
-      command: () => this.scrollToSection('hero')
-    },
-    {
-      label: 'About',
-      icon: 'pi pi-user',
-      command: () => this.scrollToSection('about')
-    },
-    {
-      label: 'Skills',
-      icon: 'pi pi-cog',
-      command: () => this.scrollToSection('skills')
-    },
-    {
-      label: 'Experience',
-      icon: 'pi pi-briefcase',
-      command: () => this.scrollToSection('experience')
-    },
-    {
-      label: 'Projects',
-      icon: 'pi pi-folder',
-      command: () => this.scrollToSection('projects')
-    },
-    {
-      label: 'Publications',
-      icon: 'pi pi-book',
-      command: () => this.scrollToSection('publications')
-    },
-    {
-      label: 'Education',
-      icon: 'pi pi-graduation-cap',
-      command: () => this.scrollToSection('education')
-    },
-    {
-      label: 'Awards',
-      icon: 'pi pi-trophy',
-      command: () => this.scrollToSection('awards')
-    },
-    {
-      label: 'Certifications',
-      icon: 'pi pi-verified',
-      command: () => this.scrollToSection('certifications')
+      items: [
+        [
+          {
+            label: 'Overview',
+            items: [
+              {
+                label: 'Home',
+                icon: 'pi pi-home',
+                command: () => this.scrollToSection('hero')
+              },
+              {
+                label: 'About Me',
+                icon: 'pi pi-user',
+                command: () => this.scrollToSection('about')
+              },
+              {
+                label: 'Skills',
+                icon: 'pi pi-cog',
+                command: () => this.scrollToSection('skills')
+              }
+            ]
+          }
+        ],
+        [
+          {
+            label: 'Professional',
+            items: [
+              {
+                label: 'Experience',
+                icon: 'pi pi-briefcase',
+                command: () => this.scrollToSection('experience')
+              },
+              {
+                label: 'Projects',
+                icon: 'pi pi-folder',
+                command: () => this.scrollToSection('projects')
+              },
+              {
+                label: 'Publications',
+                icon: 'pi pi-book',
+                command: () => this.scrollToSection('publications')
+              }
+            ]
+          }
+        ],
+        [
+          {
+            label: 'Achievements',
+            items: [
+              {
+                label: 'Education',
+                icon: 'pi pi-graduation-cap',
+                command: () => this.scrollToSection('education')
+              },
+              {
+                label: 'Awards',
+                icon: 'pi pi-trophy',
+                command: () => this.scrollToSection('awards')
+              },
+              {
+                label: 'Certifications',
+                icon: 'pi pi-verified',
+                command: () => this.scrollToSection('certifications')
+              }
+            ]
+          }
+        ]
+      ]
     },
     {
       label: 'Contact',
